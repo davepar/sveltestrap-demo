08fd5a8f8f504c764aaa447483a59a5aebc937c3
Dave Parsons <dualrudder@gmail.com>
2022-05-28 19:32:33 +0200

    Switch to using vanilla Bootstrap and Headless UI

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,9 +12,15 @@
 </script>
 
 <script lang="ts">
-  import { Button, Table, Modal, ModalFooter, Form, FormGroup, Label, Input } from 'sveltestrap';
+  import { fade } from 'svelte/transition';
   import type { Customer } from './customers';
   import { Frequency, Region, FREQUENCY_OPTIONS, REGION_OPTIONS } from './customers';
+  import {
+    Dialog,
+    DialogOverlay,
+    DialogTitle,
+    DialogDescription,
+  } from "@rgossiaux/svelte-headlessui";
 
   export let data: {
     customers: Customer[];
@@ -28,10 +34,6 @@
 
   function closeDialog() {
     dialogOpen = false;
-  }
-
-  function toggle() {
-    dialogOpen = !dialogOpen;
   }
 
   function saveCustomer() {
@@ -49,8 +51,8 @@
 </script>
 
 <svelte:head>
-  <title>Svelte with Sveltestrap</title>
-  <meta name="description" content="A demo of the Sveltestrap UI library" />
+  <title>Svelte with Bootstrap and Svelte Headless UI</title>
+  <meta name="description" content="A demo of the Bootstrap CSS library" />
   <link
     rel="stylesheet"
     href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
@@ -61,21 +63,20 @@
 
 <div class="container">
   <div>
-    <h1>Sample page for Svelte with Sveltestrap</h1>
+    <h1>Sample page for Svelte with Bootstrap CSS and Svelte Headless UI</h1>
     <p>Visit <a href="https://kit.svelte.dev">web.site.here</a> for more info</p>
   </div>
   <div class="mb-3">
-    <!-- <button type="button" class="btn btn-primary">Add Customer</button> -->
-    <Button color="primary" on:click={openDialog}>Add Customer</Button>
+    <button type="button" class="btn btn-primary" on:click={openDialog}>Add Customer</button>
   </div>
   <div>
-    <Table bordered style="width: inherit">
+    <table class="table table-bordered" style="width: inherit">
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Region</th>
-          <th>Frequency</th>
-          <th>Newsletter</th>
+          <th scope="col">Name</th>
+          <th scope="col">Region</th>
+          <th scope="col">Frequency</th>
+          <th scope="col">Newsletter</th>
         </tr>
       </thead>
       <tbody>
@@ -88,40 +89,59 @@
           </tr>
         {/each}
       </tbody>
-    </Table>
+    </table>
   </div>
 </div>
 
-<Modal isOpen={dialogOpen} {toggle} header="Add customer" body>
-  <Form>
-    <FormGroup>
-      <Label for="name">Name</Label>
-      <Input type="text" id="name" bind:value={newCustomer.name} placeholder="Full name" />
-    </FormGroup>
+<Dialog open={dialogOpen} on:close={closeDialog}>
+  <div class="modal show" style="display: block">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <DialogTitle class="modal-title">Add customer</DialogTitle>
+          <button type="button" class="btn-close" aria-label="Close" on:click={closeDialog}></button>
+        </div>
+        <div class="modal-body">
 
-    <FormGroup>
-      <Label for="region">Region</Label>
-      <Input type="select" bind:value={newCustomer.region} id="region">
-        {#each REGION_OPTIONS as { label, value }}
-          <option {value}>{label}</option>
-        {/each}
-      </Input>
-    </FormGroup>
+          <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" class="form-control" id="name" placeholder="Full name" bind:value={newCustomer.name}>
+          </div>
 
-    <FormGroup>
-      <Label>Frequency</Label>
-      {#each FREQUENCY_OPTIONS as { label, value }}
-        <Input type="radio" bind:group={newCustomer.frequency} {label} {value} />
-      {/each}
-    </FormGroup>
+          <div class="mb-3">
+            <label for="region" class="form-label">Region</label>
+            <select class="form-select" id="region" bind:value={newCustomer.region}>
+              {#each REGION_OPTIONS as { label, value }}
+                <option {value}>{label}</option>
+              {/each}
+            </select>
+          </div>
 
-    <FormGroup>
-      <Input type="checkbox" label="Newsletter" bind:checked={newCustomer.newsletter} />
-    </FormGroup>
-  </Form>
+          <div class="mb-3">
+            <p class="form-label">Frequency</p>
+            {#each FREQUENCY_OPTIONS as { label, value }}
+              <div class="form-check">
+                <input class="form-check-input" type="radio" name="frequency" id={label} bind:group={newCustomer.frequency} {value}>
+                <label class="form-check-label" for={label}>
+                  {label}
+                </label>
+              </div>
+            {/each}
+          </div>
 
-  <ModalFooter>
-    <Button color="primary" on:click={saveCustomer}>Save changes</Button>
-    <Button color="secondary" on:click={closeDialog}>Cancel</Button>
-  </ModalFooter>
-</Modal>
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" id="newsletter" bind:checked={newCustomer.newsletter} />
+              <label class="form-check-label" for="newsletter">
+                Newsletter
+              </label>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" on:click={closeDialog}>Cancel</button>
+          <button type="button" class="btn btn-primary" on:click={saveCustomer}>Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <DialogOverlay class="modal-backdrop show" />
+</Dialog>
